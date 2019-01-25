2423aad0764a5de810b95456b93e53089c55688f
Yada <ydt.hran2@gmail.com>
2019-01-25 22:21:42 +0900

    ランキング3種に対応

--- main.go
+++ main.go
@@ -9,18 +9,43 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"strconv"
+	"sync"
 )
 
-func GetChampionScore() (user string, score int) {
+type IM int
+
+const (
+	Roma    IM = 0
+	Kana    IM = 1
+	English IM = 2
+)
+
+func (im IM) String() string {
+	return map[IM]string{
+		Roma:    "ローマ字",
+		Kana:    "かな",
+		English: "英語",
+	}[im]
+}
+
+func getWordNameParamT(im IM) string {
+	return map[IM]string{
+		Roma:    "trysc.trysc.trysc.std.0",
+		Kana:    "trysc.trysc.trysc.kana.1",
+		English: "trysc.trysc.trysc.std.2",
+	}[im]
+}
+
+func fetchChampionScore(im IM) (user string, score int) {
 	jar, err := cookiejar.New(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	client := &http.Client{Jar: jar}
 	// 先週
-	// res, err := client.Get("https://www.e-typing.ne.jp/ranking/index.asp?im=1&sc=trysc&ct=-1")
+	// res, err := client.Get(fmt.Sprintf("https://www.e-typing.ne.jp/ranking/index.asp?im=%d&sc=trysc&ct=-1", im))
 	// 今週
-	res, err := client.Get("https://www.e-typing.ne.jp/ranking/index.asp?im=1&sc=trysc")
+	res, err := client.Get(fmt.Sprintf("https://www.e-typing.ne.jp/ranking/index.asp?im=%d&sc=trysc", im))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,11 +71,11 @@ func GetChampionScore() (user string, score int) {
 	return user, score
 }
 
-func GetWordName() (wordName string) {
+func fetchWordName(im IM) (wordName string) {
 	client := &http.Client{}
 	res, err := client.PostForm("https://www.e-typing.ne.jp/parts/cgilib/get_typing_setting.asp", url.Values{
 		"d_id": []string{""},
-		"t":    []string{"trysc.trysc.trysc.std.0"},
+		"t":    []string{getWordNameParamT(im)},
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -69,22 +94,37 @@ func GetWordName() (wordName string) {
 	return
 }
 
-func main() {
+func tweetRanking(im IM) {
 	scoreChan := make(chan int, 1)
 	userChan := make(chan string, 1)
 	wordChan := make(chan string, 1)
 	go func() {
-		u, s := GetChampionScore()
+		u, s := fetchChampionScore(im)
 		userChan <- u
 		scoreChan <- s
 	}()
 	go func() {
-		w := GetWordName()
+		w := fetchWordName(im)
 		wordChan <- w
 	}()
+
 	word := <-wordChan
 	user := <-userChan
 	score := <-scoreChan
-	message := fmt.Sprintf("今週の腕試し（かな）の1位は %dpt で %s さんです。（%s）", score, user, word)
+	message := fmt.Sprintf("今週の腕試し（%s）の1位は %dpt で %s さんです。（%s）",
+		im, score, user, word)
 	pkg.Tweet(message)
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+	ims := []IM{Roma, Kana, English}
+	for _, im := range ims {
+		wg.Add(1)
+		go func(im2 IM) {
+			defer wg.Done()
+			tweetRanking(im2)
+		}(im)
+	}
+	wg.Wait()
 }
